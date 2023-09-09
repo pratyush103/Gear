5cb8db29a7fc74dae75bad3a9a23b7beaa78bd7f
nikhiljoshi1012 <nikhiljoshi1012@gmail.com>
2023-09-09 07:18:57 +0530

    Started work on the sidebar
    Work started on the Channellistcontainer and imported an assets folder that i had to download and put under src  folder

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -1,4 +1,21 @@
+//Side bar for the chat application
 import React from 'react';
+import {Channellist, useCharContext} from 'stream-chat';
+import Cookies from 'universal-cookie';
+import{ChannelSearch,TeamChannelList,TeamChannelPreview} from './';
+import HospitalIcon from '../assets/hospital.png'
+
+const SideBar= () => (
+    <div className="Channel-list__sidebar">
+        <div className="Channel-list__sidebar__icon1">
+            <div className="icon1__inner">
+                <img src= {HospitalIcon} alt="Hospital" width="30"/>
+            </div>
+
+        </div>
+
+    </div>
+)
 const ChannelListContainer =() => {
     return (
         <div>ChannelListContainer
