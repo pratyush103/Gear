6b4e8a1421b72b196fd2f72161926ae45c64fd8d
Pratyush Landekar <pratyushlandekar103@gmail.com>
2023-10-11 01:24:44 +0530

    fixed err: signup

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -3,9 +3,7 @@ import Cookies from 'universal-cookie';
 import axios from 'axios';
 
 import signinImage from '../assets/signup.jpg';
-
-import spinningGear from '../assets/spinning_gear.gif'
-
+import spinningGear from '../assets/spinning_gear.gif';
 import './Signin-up.css';
 
 const cookies = new Cookies();
@@ -34,25 +32,29 @@ const Auth = () => {
 
     const URL = 'http://localhost:5000/auth';
 
-    const {
-      data: { token, userId, hashedPassword, fullName },
-    } = await axios.post(`${URL}/signup`, {
-      username,
-      password,
-      fullName: form.fullName,
-      phoneNumber,
-      avatarURL,
-    });
+    try {
+      const response = await axios.post(`${URL}/signup`, {
+        username,
+        password,
+        fullName: form.fullName,
+        phoneNumber,
+        avatarURL,
+      });
 
-    cookies.set('token', token);
-    cookies.set('username', username);
-    cookies.set('fullName', fullName);
-    cookies.set('userId', userId);
-    cookies.set('phoneNumber', phoneNumber);
-    cookies.set('avatarURL', avatarURL);
-    cookies.set('hashedPassword', hashedPassword);
+      const { token, userId, hashedPassword, fullName } = response.data;
 
-    window.location.reload();
+      cookies.set('token', token);
+      cookies.set('username', username);
+      cookies.set('fullName', fullName);
+      cookies.set('userId', userId);
+      cookies.set('phoneNumber', phoneNumber);
+      cookies.set('avatarURL', avatarURL);
+      cookies.set('hashedPassword', hashedPassword);
+
+      window.location.reload();
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const handleLoginSubmit = async (e) => {
@@ -62,40 +64,38 @@ const Auth = () => {
 
     const URL = 'http://localhost:5000/auth';
 
-    const {
-      data: { token, userId, fullName },
-    } = await axios.post(`${URL}/login`, {
-      username,
-      password,
-    });
+    try {
+      const response = await axios.post(`${URL}/login`, {
+        username,
+        password,
+      });
 
-    cookies.set('token', token);
-    cookies.set('username', username);
-    cookies.set('fullName', fullName);
-    cookies.set('userId', userId);
+      const { token, userId, fullName } = response.data;
 
-    window.location.reload();
+      cookies.set('token', token);
+      cookies.set('username', username);
+      cookies.set('fullName', fullName);
+      cookies.set('userId', userId);
+
+      window.location.reload();
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const switchMode = () => {
-    
-        setTimeout(() => {
-          setIsSignup((prevIsSignup) => !prevIsSignup);
-        }, 2000); // Adjust the delay time (in milliseconds) as needed.
-      
-      
+    setTimeout(() => {
+      setIsSignup((prevIsSignup) => !prevIsSignup);
+    }, 2000); // Adjust the delay time (in milliseconds) as needed.
+
     console.log(isSignup);
 
-    const wrapper = document.querySelector(".wrapper");
-    if(isSignup){
-        wrapper.classList.remove("active");
-
+    const wrapper = document.querySelector('.wrapper');
+    if (isSignup) {
+      wrapper.classList.remove('active');
+    } else {
+      wrapper.classList.add('active');
     }
-    else{
-        wrapper.classList.add("active");
-        
-    }
-
   };
 
   return (
@@ -131,7 +131,11 @@ const Auth = () => {
               <label htmlFor="password">Password</label>
               <i className="bx bxs-lock-alt"></i>
             </div>
-            <button type="submit" className="btn animation" style={{ '--i': 3, '--j': 24 }}>
+            <button
+              type="submit"
+              className="btn animation"
+              style={{ '--i': 3, '--j': 24 }}
+            >
               Login
             </button>
             <div className="logreg-link animation" style={{ '--i': 4, '--j': 25 }}>
@@ -163,47 +167,69 @@ const Auth = () => {
               Sign Up
             </h2>
             <form onSubmit={handleSignupSubmit}>
-                    <div className="input-box animation" style={{ '--i': 18, '--j': 1 }}>
-                        <input type="text" required />
-                        <label htmlFor="fullName">Full Name</label>
-                        <i className="bx bxs-user"></i>
-                    </div>
-                    <div className="input-box animation" style={{ '--i': 18, '--j': 1 }}>
-                        <input type="text" required />
-                        <label htmlFor="username">Username</label>
-                        <i className="bx bxs-user"></i>
-                    </div>
-                    <div className="input-box animation" style={{ '--i': 19, '--j': 2 }}>
-                        <input type="text" required />
-                        <label htmlFor="phoneNumber">Phone No.</label>
-                        <i className="bx bxs-phone-call"></i>
-                    </div>
-                    <div className="input-box animation" style={{ '--i': 20, '--j': 3 }}>
-                        <input type="text" required />
-                        <label htmlFor="avatarURL">Avatar URL</label>
-                        <i className="bx bxs-image"></i>
-                    </div>
-                    <div className="input-box animation" style={{ '--i': 20, '--j': 3 }}>
-                        <input type="password" name="Phone" required />
-                        <label htmlFor="password">Password</label>
-                        <i className="bx bxs-lock-alt"></i>
-                    </div>
-                    <div className="input-box animation" style={{ '--i': 21, '--j': 4 }}>
-                        <input type="password" required />
-                        <label htmlFor="confirmPassword">Confirm Password</label>
-                        <i className="bx bxs-lock-alt"></i>
-                    </div>
-                    <button type="submit" className="btn animation" style={{ '--i': 21, '--j': 4 }}>Sign Up</button>
-                    <div className="logreg-link animation" style={{ '--i': 22, '--j': 5 }}>
-                        <p onClick={switchMode}>Already have an account? <a href="#" className="login-link">Login</a></p>
-                    </div>
-                    </form>
+              <div className="input-box animation" style={{ '--i': 18, '--j': 1 }}>
+                <input type="text" name="fullName" value={form.fullName} onChange={handleChange} required />
+                <label htmlFor="fullName">Full Name</label>
+                <i className="bx bxs-user"></i>
+              </div>
+              <div className="input-box animation" style={{ '--i': 18, '--j': 1 }}>
+                <input type="text" name="username" value={form.username} onChange={handleChange} required />
+                <label htmlFor="username">Username</label>
+                <i className="bx bxs-user"></i>
+              </div>
+              <div className="input-box animation" style={{ '--i': 19, '--j': 2 }}>
+                <input type="text" name="phoneNumber" value={form.phoneNumber} onChange={handleChange} required />
+                <label htmlFor="phoneNumber">Phone No.</label>
+                <i className="bx bxs-phone-call"></i>
+              </div>
+              <div className="input-box animation" style={{ '--i': 20, '--j': 3 }}>
+                <input type="text" name="avatarURL" value={form.avatarURL} onChange={handleChange} required />
+                <label htmlFor="avatarURL">Avatar URL</label>
+                <i className="bx bxs-image"></i>
+              </div>
+              <div className="input-box animation" style={{ '--i': 20, '--j': 3 }}>
+                <input type="password" name="password" value={form.password} onChange={handleChange} required />
+                <label htmlFor="password">Password</label>
+                <i className="bx bxs-lock-alt"></i>
+              </div>
+              <div className="input-box animation" style={{ '--i': 21, '--j': 4 }}>
+                <input
+                  type="password"
+                  name="confirmPassword"
+                  value={form.confirmPassword}
+                  onChange={handleChange}
+                  required
+                />
+                <label htmlFor="confirmPassword">Confirm Password</label>
+                <i className="bx bxs-lock-alt"></i>
+              </div>
+              <button
+                type="submit"
+                className="btn animation"
+                style={{ '--i': 21, '--j': 4 }}
+              >
+                Sign Up
+              </button>
+              <div className="logreg-link animation" style={{ '--i': 22, '--j': 5 }}>
+                <p onClick={switchMode}>
+                  Already have an account? <a href="#" className="login-link">Login</a>
+                </p>
+              </div>
+            </form>
           </div>
         )}
         <div className="info-text register">
           <h1 className="animation" style={{ '--i': 17, '--j': 0 }}>
-            Welcome to Gear! 
-          </h1><img src={spinningGear} className="gear-spinning animation" style={{ '--i': 17, '--j': 0 }} width="50" height="50" alt="sign in" />
+            Welcome to Gear!
+          </h1>
+          <img
+            src={spinningGear}
+            className="gear-spinning animation"
+            style={{ '--i': 17, '--j': 0 }}
+            width="50"
+            height="50"
+            alt="sign in"
+          />
           <p className="animation" style={{ '--i': 18, '--j': 1 }}>
             Lorem ipsum dolor sit amet consectetur adipisicing.
           </p>
